cfd6446f08d0b88417fa44c331b75af58002998b
Chika <chika.hamayama@gmail.com>
2018-12-11 01:15:43 +0900

    mk update action

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get 'posts/:id' => 'posts#show'
   post 'posts/create'
   get 'posts/:id/edit' => 'posts#edit'
-
+  post 'posts/:id/update' => 'posts#update'
   get '/' => "home#top"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'about' => "home#about"
